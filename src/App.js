833b4bfed921f9c213338f56fec06b1b7ac50906
Ekarizqi <eka30riski@gmail.com>
2022-05-31 16:00:47 +0700

    counter-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+import './App.css';
+import Counter from './Counter';
+//import { FunctionalCounter } from './pages/FunctionalCounter';
+// import Counter from './pages/ClassCounter';
+
+function App() {
+
+  return (
+    <div className='container'>
+      <h2>Counter App</h2>
+      <Counter/>
+     </div>
+  );
+}
+
+export default App;
